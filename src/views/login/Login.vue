f1bf3d3faf52cbd827dabad06387323cd06da8fc
Marcos Emanuel <marcos.length@gmail.com>
2021-08-15 14:50:54 -0300

    add form validation to login page

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -3,7 +3,7 @@
     <v-col cols="12" md="5" lg="5" align="center">
     <v-img max-width="150" class="mt-n16 mb-5" src="../../assets/logo-min.png"></v-img>
 
-    <v-form v-model="valid">
+    <v-form v-model="valid" ref="login_form">
       <v-text-field
         v-model="user.email"
         label="E-mail"
@@ -11,6 +11,7 @@
         append-icon="mdi-email"
         outlined
         dense
+        :rules="emailRules"
       >
       </v-text-field>
       <v-text-field
@@ -21,6 +22,8 @@
         outlined
         dense
         type="password"
+        :counter="counter"
+        :rules="nameRules"
       >
       </v-text-field>
       <v-row justify="end" class="px-3">
@@ -33,11 +36,27 @@
           </v-btn>
         </v-col>
         <v-col cols="6">
-          <v-btn @click="login" block color="primary">
+          <v-btn 
+            @click="login" 
+            block 
+            color="primary"
+            :loading="loading"
+            :disabled="loading"
+          >
             <span>Entrar</span>
           </v-btn>
         </v-col>
       </v-row>
+      <v-snackbar color="error" :timeout="6000" v-model="error">
+        <span>
+          {{ errorMessage }}
+        </span>
+        <template v-slot:action="{ attrs }">
+          <v-btn color="white" text v-bind="attrs" @click="closeError">
+            Fechar
+          </v-btn>
+        </template>
+      </v-snackbar>
     </v-form>
 
     </v-col>
@@ -48,26 +67,51 @@
 import Vue from "vue";
 import LoginRequest from "./../../requests/LoginRequest.js";
   export default Vue.extend({
-    data: () => ({
+    data: (instance) => ({
       valid: null,
+      error: null,
+      errorMessage: null,
+      loading: null,
+      counter: 0,
+      maxPassword: 8,
       user: {
         email: null,
         password: null
-      }
+      },
+      emailRules: [
+        v => !!v || 'O campo e-mail é obrigatório',
+        v => /.+@.+\..+/.test(v) || 'E-mail inválido',
+      ],
+      nameRules: [
+        v => !!v || 'O campo senha é obrigatório',
+        v => (v || '').length >= instance.maxPassword || `A senha precisa ter pelo menos ${instance.maxPassword} caracteres`
+      ]
     }),
     methods: {
       async login() {
         try {
-          const loginRequest = new LoginRequest (this.user.email, this.user.password)
-          const loginResponse = await loginRequest.send()
+          if (this.$refs.login_form.validate()) {
+            const loginRequest = new LoginRequest (this.user.email, this.user.password)
+            const loginResponse = await loginRequest.send()
 
-          this.$store.dispatch('actionSetUser', loginResponse.data.user)
-          this.$store.dispatch('actionSaveUser')
+            this.$store.dispatch('actionSetUser', loginResponse.data.user)
+            this.$store.dispatch('actionSaveUser')
 
-          this.$router.push('/home')
+            this.$router.push('/home')
+          } else {
+            this.errorMessage = 'Campos Inválidos'
+            this.error = true
+            this.loading = false
+          }   
         } catch (error) {
-          console.log(error.response.data.message)
+          this.loading = false
+          this.error = true
+          this.errorMessage = error.response.data.message ?? 'Ocorreu um erro inesperado. Por favor tente novamente mais tarde.'
         }
+      },
+      closeError() {
+        this.error = false
+        this.loading = false
       }
     }
   })
